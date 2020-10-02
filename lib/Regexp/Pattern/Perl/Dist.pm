0db93b6dcdc68607c931466a0043339016b01a01
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2020-10-02 10:23:40 +0700

    [bugfix] Do not capture group in perl_distname

--- lib/Regexp/Pattern/Perl/Dist.pm
+++ lib/Regexp/Pattern/Perl/Dist.pm
@@ -10,7 +10,7 @@ use strict;
 
 our %RE = (
     perl_distname => {
-        pat => '\A[A-Za-z_][A-Za-z_0-9]*(-[A-Za-z_0-9]+)*\z',
+        pat => '\A[A-Za-z_][A-Za-z_0-9]*(?:-[A-Za-z_0-9]+)*\z',
         examples => [
             {str=>'', matches=>0},
             {str=>'Foo-Bar', matches=>1},
